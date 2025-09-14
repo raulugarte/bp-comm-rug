e176b7f211a3241ed3876bad7c10eac06eba55b9
raulugarte <63106469+raulugarte@users.noreply.github.com>
2025-09-14 15:37:50 +0200

    Update teaser.js

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -1,7 +1,10 @@
+/* 
 import {
   div, a, span, img, video, source, button,
   h1,
 } from '../../scripts/dom-helpers.js';
+*/
+
 import { readBlockConfig } from '../../scripts/aem.js';
 
 const prefersReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)');
