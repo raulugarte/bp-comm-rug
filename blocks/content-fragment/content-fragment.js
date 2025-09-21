db2763df349eb0d22ba76ef5955e4c7d49af089a
raulugarte <63106469+raulugarte@users.noreply.github.com>
2025-09-21 23:37:07 +0200

    Update content-fragment.js

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -18,7 +18,7 @@ export default async function decorate(block) {
 
 
   
-  const cfReq = await fetch(url)
+  const cfReq = await fetch(url,options)
     .then((response) => response.json())
     .then((contentfragment) => {
       let offer = '';
@@ -31,20 +31,7 @@ export default async function decorate(block) {
   const itemId = `urn:aemconnection:${offerpath}/jcr:content/data/master`;
 
 
-  /* RUG 
-  
-  block.innerHTML = `
-  <div class='banner-content' data-aue-resource=${itemId} data-aue-label="offer content fragment" data-aue-type="reference" data-aue-filter="cf">
-      <div data-aue-prop="heroImage" data-aue-label="hero image" data-aue-type="media" class='banner-detail' style="background-image: linear-gradient(90deg,rgba(0,0,0,0.6), rgba(0,0,0,0.1) 80%) ,url(${aempublishurl + cfReq.heroImage._dynamicUrl});">
-          <p data-aue-prop="headline" data-aue-label="headline" data-aue-type="text" class='pretitle'>${cfReq.headline}</p>
-          <p data-aue-prop="pretitle" data-aue-label="pretitle" data-aue-type="text" class='headline'>${cfReq.pretitle}</p>
-          <p data-aue-prop="detail" data-aue-label="detail" data-aue-type="richtext" class='detail'>${cfReq.detail.plaintext}</p>
-      </div>
-      <div class='banner-logo'>
-      </div>
-  </div>
-`;
-*/
+
 
   // Compose _dynamicUrl, assuming it exists
   const imageBase = aempublishurl + cfReq.recipeImage._dynamicUrl;
