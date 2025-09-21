ebbf3b3e20d97db6b76be0b5c4ef385d8b1629fa
raulugarte <63106469+raulugarte@users.noreply.github.com>
2025-09-21 22:45:12 +0200

    Update offer.js

--- blocks/offer/offer.js
+++ blocks/offer/offer.js
@@ -11,6 +11,9 @@ export default async function decorate(block) {
     : `${aempublishurl}${persistedquery};path=${offerpath};variation=${variationname};ts=${Math.random() * 1000}`;
   const options = { credentials: 'include' };
 
+const cf = await fetch(url).then(res => res.json());
+const item = cf.data.offerByPath.item;
+  
   const cfReq = await fetch(url, options)
     .then((response) => response.json())
     .then((contentfragment) => {
